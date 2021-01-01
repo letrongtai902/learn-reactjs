3a9eed0c73fefcb3af0647d15a7d372d41b49013
Lê Trọng Tài <62075154+letrongtai902@users.noreply.github.com>
2021-01-01 15:55:31 +0700

    commit fix bug

--- src/features/Auth/components/RegisterForm/index.jsx
+++ src/features/Auth/components/RegisterForm/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+// import { useForm } from 'react-hook-form';
+// import { yupResolver } from '@hookform/resolvers/yup';
+//import * as yup from 'yup';
 import { Avatar, Typography } from '@material-ui/core';
 import { LockOutlined } from '@material-ui/icons';
 
@@ -11,22 +11,22 @@ RegisterForm.propTypes = {
 };
 
 function RegisterForm(props) {
-  const schema = yup.object().shape({
-    title: yup.string().required('Nhập title').min(5, 'nhập 5 ký tự trở lên'),
-  });
-  const form = useForm({
-    defaultValues: {
-      title: '',
-    },
-    resolver: yupResolver(schema),
-  });
-  const handleSubmit = (value) => {
-    const { onSubmit } = props;
-    if (onSubmit) {
-      onSubmit(value);
-    }
-    form.reset();
-  };
+  // const schema = yup.object().shape({
+  //   title: yup.string().required('Nhập title').min(5, 'nhập 5 ký tự trở lên'),
+  // });
+  // const form = useForm({
+  //   defaultValues: {
+  //     title: '',
+  //   },
+  //   resolver: yupResolver(schema),
+  // });
+  // const handleSubmit = (value) => {
+  //   const { onSubmit } = props;
+  //   if (onSubmit) {
+  //     onSubmit(value);
+  //   }
+  //   form.reset();
+  // };
   return (
     <div>
       <Avatar>
