bc8cdd4d7b62fbfebcb044b729207383ba52a355
Lê Trọng Tài <62075154+letrongtai902@users.noreply.github.com>
2021-01-03 17:52:18 +0700

    commit lazyloading

--- src/App.js
+++ src/App.js
@@ -1,43 +1,51 @@
 import './App.scss';
-import Home from './features/Home/pages';
+// import Home from './features/Home/pages';
 import Footer from './components/Footer';
 import { NavLink, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import HeaderLinks from './components/Header/HeaderLinks';
 import logo from './assets/img/logo.png';
-import Blog from './features/Blog/pages';
-import About from './features/About/pages';
-import Contact from './features/Contact/pages';
+import Loading from './components/Loading';
+// import Blog from './features/Blog/pages';
+// import About from './features/About/pages';
+// import Contact from './features/Contact/pages';
+import { lazy, Suspense } from 'react';
 
 const dashboardRoutes = [];
+const Homelz = lazy(() => import('./features/Home/pages'));
+const Bloglz = lazy(() => import('./features/Blog/pages'));
+const Aboutz = lazy(() => import('./features/About/pages'));
+const Contactlz = lazy(() => import('./features/Contact/pages'));
 
 function App(props) {
   const { ...rest } = props;
   return (
     <div>
-      <Header
-        color="transparent"
-        routes={dashboardRoutes}
-        brand={
-          <NavLink to="/">
-            <img src={logo} style={{ height: '60px' }} alt="Logo UMai" />
-          </NavLink>
-        }
-        rightLinks={<HeaderLinks />}
-        fixed
-        changeColorOnScroll={{
-          height: 10,
-          color: 'success',
-        }}
-        {...rest}
-      />
-      <Switch>
-        <Route path="/" component={Home} exact />
-        <Route path="/blogs" component={Blog} />
-        <Route path="/abouts" component={About} />
-        <Route path="/contacts" component={Contact} />
-      </Switch>
-      <Footer />
+      <Suspense fallback={<Loading />}>
+        <Header
+          color="transparent"
+          routes={dashboardRoutes}
+          brand={
+            <NavLink to="/">
+              <img src={logo} style={{ height: '60px' }} alt="Logo UMai" />
+            </NavLink>
+          }
+          rightLinks={<HeaderLinks />}
+          fixed
+          changeColorOnScroll={{
+            height: 10,
+            color: 'success',
+          }}
+          {...rest}
+        />
+        <Switch>
+          <Route path="/" component={Homelz} exact />
+          <Route path="/blogs" component={Bloglz} />
+          <Route path="/abouts" component={Aboutz} />
+          <Route path="/contacts" component={Contactlz} />
+        </Switch>
+        <Footer />
+      </Suspense>
     </div>
   );
 }
